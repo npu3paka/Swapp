f58e97a913a1286adc512974445d95fbf566b6d2
Altimir Antonov <altimir.antonov@158ltd.com>
2015-09-13 13:30:40 +0300

    Add On Board

--- Swapp/Swapp/AppDelegate.m
+++ Swapp/Swapp/AppDelegate.m
@@ -7,17 +7,94 @@
 //
 
 #import "AppDelegate.h"
+#import "OnboardingViewController.h"
+#import "OnboardingContentViewController.h"
+#import "LoginViewController.h"
 
 @interface AppDelegate ()
 
 @end
 
+static NSString * const kUserHasOnboardedKey = @"user_has_onboarded";
+
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    // determine if the user has onboarded yet or not
+    BOOL userHasOnboarded = [[NSUserDefaults standardUserDefaults] boolForKey:kUserHasOnboardedKey];
+    
+    // if the user has already onboarded, just set up the normal root view controller
+    // for the application
+    if (userHasOnboarded) {
+        [self setupNormalRootViewController];
+    }
+    
+    // otherwise set the root view controller to the onboarding view controller
+    else {
+        self.window.rootViewController = [self generateFirstDemoVC];
+    }
+    
+    application.statusBarStyle = UIStatusBarStyleLightContent;
+    
+    [self.window makeKeyAndVisible];
     // Override point for customization after application launch.
     return YES;
+}
+
+- (void)setupNormalRootViewController {
+    
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *vc = [sb instantiateViewControllerWithIdentifier:@"loginView"];
+    vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    
+    self.window.rootViewController = vc;
+}
+
+- (void)handleOnboardingCompletion {
+    // set that we have completed onboarding so we only do it once... for demo
+    // purposes we don't want to have to set this every time so I'll just leave
+    // this here...
+    //    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kUserHasOnboardedKey];
+    
+    // transition to the main application
+    [self setupNormalRootViewController];
+}
+
+
+- (OnboardingViewController *)generateFirstDemoVC {
+    OnboardingContentViewController *firstPage = [OnboardingContentViewController contentWithTitle:@"What A Beautiful Photo" body:@"This city background image is so beautiful." image:[UIImage imageNamed:@"blue"] buttonText:@"Enable Location Services" action:^{
+        [[[UIAlertView alloc] initWithTitle:nil message:@"Here you can prompt users for various application permissions, providing them useful information about why you'd like those permissions to enhance their experience, increasing your chances they will grant those permissions." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }];
+    
+    OnboardingContentViewController *secondPage = [OnboardingContentViewController contentWithTitle:@"I'm so sorry" body:@"I can't get over the nice blurry background photo." image:[UIImage imageNamed:@"red"] buttonText:@"Connect With Facebook" action:^{
+        [[[UIAlertView alloc] initWithTitle:nil message:@"Prompt users to do other cool things on startup. As you can see, hitting the action button on the prior page brought you automatically to the next page. Cool, huh?" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }];
+    secondPage.movesToNextViewController = YES;
+    secondPage.viewDidAppearBlock = ^{
+        [[[UIAlertView alloc] initWithTitle:@"Welcome!" message:@"You've arrived on the second page, and this alert was displayed from within the page's viewDidAppearBlock." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    };
+    
+    OnboardingContentViewController *thirdPage = [OnboardingContentViewController contentWithTitle:@"Seriously Though" body:@"Kudos to the photographer." image:[UIImage imageNamed:@"yellow"] buttonText:@"Get Started" action:^{
+        [self handleOnboardingCompletion];
+    }];
+    
+    OnboardingViewController *onboardingVC = [OnboardingViewController onboardWithBackgroundImage:[UIImage imageNamed:@"street"] contents:@[firstPage, secondPage, thirdPage]];
+    onboardingVC.shouldFadeTransitions = YES;
+    onboardingVC.fadePageControlOnLastPage = YES;
+    onboardingVC.fadeSkipButtonOnLastPage = YES;
+    
+    // If you want to allow skipping the onboarding process, enable skipping and set a block to be executed
+    // when the user hits the skip button.
+    onboardingVC.allowSkipping = YES;
+    onboardingVC.skipHandler = ^{
+        [self handleOnboardingCompletion];
+    };
+    
+    return onboardingVC;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
---
+++ Swapp/Swapp/Controllers/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Swapp
+//
+//  Created by Altimir Antonov on 9/13/15.
+//  Copyright (c) 2015 Altimir Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
