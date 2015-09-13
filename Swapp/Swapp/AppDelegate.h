9fd1ab7fcd92d3e294ab06545f21dff7e630de15
Altimir Antonov <altimir.antonov@158ltd.com>
2015-09-13 13:12:06 +0300

    Commit

---
+++ Swapp/Swapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swapp
+//
+//  Created by Altimir Antonov on 9/13/15.
+//  Copyright (c) 2015 Altimir Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swapp/Swapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swapp
+//
+//  Created by Altimir Antonov on 9/13/15.
+//  Copyright (c) 2015 Altimir Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
